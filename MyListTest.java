cba9090d0049b6ad153076d36e6c9f4956194704
someone <someone@someplace.com>
2019-03-07 15:29:32 +0700

    some init msg

---
+++ MyListTest.java
@@ -0,0 +1,22 @@
+import java.util.Collection;
+import java.util.Arrays;
+
+
+public class MyListTest 
+{
+    public static void main(String[] args)
+    {
+        Mylist<Integer> listInteger = new Mylist<Integer>();
+        listInteger.add(1);
+        listInteger.add(2);
+        listInteger.add(3);
+        listInteger.add(4);
+
+        System.out.println("element 3: " + listInteger.get(3));
+        System.out.println("element 1: " + listInteger.get(1));
+        System.out.println("element 2: " + listInteger.get(2));
+
+        listInteger.get(-1);
+        System.out.println("element -1: " + listInteger.get(-1));
+    }    
+}
